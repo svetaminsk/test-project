9985ef236d12ae7aeefd436041639b0e3d061b2c
svetlana.evdokimova <svetlana.evdokimova@hiqo-solutions.com>
2025-02-27 19:18:45 +0300

    feat: add config

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,15 +34,10 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-/*     {
+   {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    }, */
 
     {
       name: 'webkit',
